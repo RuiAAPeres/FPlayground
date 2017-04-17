50ed34d2a30e89846cd128561beac7ae57b28ef4
ruiaaperes <rui.peres@babylonhealth.com>
2017-04-17 16:55:56 +0100

    Trying Type classes

--- Playground.hsproj/Example1.hs
+++ Playground.hsproj/Example1.hs
@@ -1,7 +1,10 @@
 module Example1 where
 
-inc1 :: Int -> Int 
+inc1 :: Num a => a -> a
 inc1 x = x + 1
+
+frac :: Fractional a => a -> a -> a  
+frac x y = (x + y) / 2 
   
 exclamation :: String -> String 
 exclamation str = str ++ "!"
